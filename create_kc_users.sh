72b1bb7d38150423907ecf5d373291387ec6238e
jok1n9 <joaquimandrade@ua.pt>
2026-03-24 21:17:35 +0000

    chore: add script to seed test users into keycloak

---
+++ create_kc_users.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Adding professor user..."
+docker exec main-keycloak-1 /opt/keycloak/bin/kcadm.sh create users -r egs -s username=professor@ua.pt -s enabled=true -s email=professor@ua.pt -s firstName=Professor -s lastName=User
+docker exec main-keycloak-1 /opt/keycloak/bin/kcadm.sh set-password -r egs --username professor@ua.pt --new-password professor
+docker exec main-keycloak-1 /opt/keycloak/bin/kcadm.sh update users/$(docker exec main-keycloak-1 /opt/keycloak/bin/kcadm.sh get users -r egs -q username=professor@ua.pt --fields id --format csv --noquotes) -r egs -s 'attributes={"role":["professor"]}'
+
+echo "Adding student user..."
+docker exec main-keycloak-1 /opt/keycloak/bin/kcadm.sh create users -r egs -s username=student@ua.pt -s enabled=true -s email=student@ua.pt -s firstName=Student -s lastName=User
+docker exec main-keycloak-1 /opt/keycloak/bin/kcadm.sh set-password -r egs --username student@ua.pt --new-password student
+docker exec main-keycloak-1 /opt/keycloak/bin/kcadm.sh update users/$(docker exec main-keycloak-1 /opt/keycloak/bin/kcadm.sh get users -r egs -q username=student@ua.pt --fields id --format csv --noquotes) -r egs -s 'attributes={"role":["student"]}'
+echo "Done!"
